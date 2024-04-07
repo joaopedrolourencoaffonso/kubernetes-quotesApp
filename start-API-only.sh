80deca2bdecbe93aabc883e4209850a2521537aa
Joao Pedro <joaoaffonso@id.uff.br>
2024-04-07 09:50:54 -0300

    Por algum motivo o quotes-api dá erro se outros pods forem inicializados muito rápido. Estou pondo esse sleep para dar tempo dele subir

--- start-API-only.sh
+++ start-API-only.sh
@@ -2,15 +2,18 @@
 
 echo "--- Iniciando pods ---"
 kubectl apply -f pod-quotes-API.yaml
+sleep 10;
 kubectl apply -f pod-quotes-DB.yaml
 
-
+sleep 10;
 echo "--- Pods funcionando ---"
 kubectl get pod
 echo "------------------------"
 
 echo "---- Iniciando serviços ----"
+sleep 10;
 kubectl apply -f ClusterIP-quotesDB.yaml
+sleep 10;
 kubectl apply -f NodePort-quotes-API.yaml
 
 echo "--- Serviços funcionando ---"
